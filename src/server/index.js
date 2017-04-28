a993b961c12f3118ed4aed5328c9118e86f326d8
Dzmitry Ramaniuk <kanumowa@gmail.com>
2017-04-28 23:32:47 +0300

    change console.log message yarn>npm run

--- src/server/index.js
+++ src/server/index.js
@@ -25,5 +25,5 @@ routing(app);
 http.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
-    '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`);
+    '(development).\nKeep "npm run dev:wds" running in an other terminal'}.`);
 });
